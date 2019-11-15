01130eb4e988dfca6679d2bff134cde8df77713b
Juan Camilo Londoño <juan.londono@travelport.com>
2019-11-15 15:20:36 +0100

    commit 13

--- bowling.spec.js
+++ bowling.spec.js
@@ -7,17 +7,19 @@ beforeEach(() => (g = new Game()));
 test("gutter game", () => {
   const pins = 0;
   const rolls = 20;
-  for (let i = 0; i < rolls; i++) {
-    g.roll(0);
-  }
+  rollMany(rolls, pins);
 
   expect(g.score()).toBe(pins);
 });
 
 test("all ones", () => {
-  for (let i = 0; i < 20; i++) {
-    g.roll(1);
-  }
+  rollMany(20, 1);
 
   expect(g.score()).toBe(20);
 });
+
+function rollMany(rolls, pins) {
+  for (let i = 0; i < rolls; i++) {
+    g.roll(pins);
+  }
+}
